412e92ddad2507c11e5154e75e15eb143d0f32bb
gustavo@mumuki.org <gustavo@mumuki.org>
2019-07-19 19:46:38 +0000

    Mumuki Export on 2019-07-19 19:46:38 +0000

--- extra.js
+++ extra.js
@@ -1,13 +1,17 @@
-var fakeConsole = {
-  lines: [],
-  log: function(line) {
-    this.lines.push(line + "\n");
-    console.log(line);
-  },
-  toString: function() {
-    return String.prototype.concat.apply([], this.lines);
-  }
-};
+var fakeConsole;
+
+function setUpFakeConsole() {
+  fakeConsole = {
+    lines: [],
+    log: function(line) {
+      this.lines.push(line + "\n");
+      console.log(line);
+    },
+    toString: function() {
+      return String.prototype.concat.apply([], this.lines);
+    }
+  };
+}
 
 function imprimir(s) {
   fakeConsole.log(s)
